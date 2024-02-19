19fe9f4a875429822c4d90dc65fbe64c1d53ed32
Skuddin <sheikh@nextgenitltd.com>
2024-02-19 14:46:26 +0600

    fetch user data

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,37 @@
+import { Link, Routes, Route } from "react-router-dom"
+
+import Home from "./pages/Home";
+import About from "./pages/About";
 
 function App() {
 
   return (
     <>
-      <h1 className="text-center">Hello</h1>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark ">
+        <div className="container-fluid ">
+          <Link className="navbar-brand" to={"/"}>Crud App</Link>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
+            <ul className="navbar-nav ">
+              <li className="nav-item">
+                <Link className="nav-link active" to={"/"}>Home</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to={"/about"}>About</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+      <div className="container">
+        <Routes>
+          <Route path={'/'} element={<Home />} />
+          <Route path={'/about'} element={<About />} />
+        </Routes>
+      </div>
     </>
   )
 }
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from 'react'
+import http from '../http'
+
+export default function Home() {
+  const [users, setUsers] = useState([]);
+  useEffect(()=>{
+    getAllUsers();
+  },[]);
+
+  const getAllUsers = () => {
+    http.get('/users').then(res => {
+      setUsers(res.data)
+    })
+
+  }
+
+  // console.log(users);
+  return (
+    <div>
+      <h3>Home Page</h3>
+      <table className='table table-bordered'>
+        <thead>
+          <tr>
+            <th>Sl</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user,index) => (
+              <tr key={user.id}>
+                <td>{ index }</td>
+                <td>{ user.name }</td>
+                <td>{ user.email }</td>
+                <td className='d-flex justify-content-center gap-2'>
+                  {/* Edit btn */}
+                  <button type='button' className='btn btn-dark'>Edit</button>
+
+                  {/* Delete btn */}
+                  <button type='button' className='btn btn-danger'>Delete</button>
+                </td>
+              </tr>
+          ))
+          }
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
